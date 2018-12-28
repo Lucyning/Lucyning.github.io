0cd90904c3aec82576905f0a4ee3c9e497b9546f
ningruoyang <ningruoyang@meituan.com>
2018-12-28 15:56:16 +0800

    修改跳转

--- src/main/mods/constant.js
+++ src/main/mods/constant.js
@@ -8,6 +8,6 @@ export const HEADLIST= [{
     className: 'watermelon'
 },{
     text: '微博',
-    linkUrl: 'https://github.com/Lucyning',
+    linkUrl: 'https://weibo.com/u/5576021527',
     className: 'strawberry'
 }]
